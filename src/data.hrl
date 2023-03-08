a8fbe0416844ed769b5c7fc7502226afee843379
Thomas Weinschenk <thomas.weinschenk@endress.com>
2023-03-08 14:42:13 +0100

    Removed commented code

--- src/data.hrl
+++ src/data.hrl
@@ -2,16 +2,12 @@
 -type account_number() :: integer().
 -type money() :: number().
 
-%-record(person,
-%    {id :: unique_id(),
-%     given_name :: binary(),
-%     surname :: binary()}).
 -record(account,
     {account_number :: account_number(),
      amount :: money()}).
--record(transfer, 
-    {id :: unique_id(), 
-     timestamp :: erlang:timestamp(), 
+-record(transfer,
+    {id :: unique_id(),
+     timestamp :: erlang:timestamp(),
      from_account_number :: account_number(),
      to_account_number :: account_number(),
      amount :: money()}).
