429eaff9a127f3c0f0a9f4acbdf1f2e5ed52038e
camelmann <68893386+camelmann@users.noreply.github.com>
2023-09-20 16:55:42 +0200

    change transfer event type

--- src/data.hrl
+++ src/data.hrl
@@ -2,24 +2,19 @@
 -type account_number() :: integer().
 -type money() :: number().
 
--record(account,
-    {account_number :: account_number(),
-     amount :: money()}).
--record(transfer, 
-    {id :: unique_id(), 
-     timestamp :: erlang:timestamp(), 
-     from_account_number :: account_number(),
-     to_account_number :: account_number(),
-     amount :: money()}).
--record(transfer_event, {
-    source :: transfer_service,
-    eventId :: non_neg_integer(),
-    accountIdSender :: non_neg_integer(),
-    accountIdReceiver :: non_neg_integer(),
-    amount :: number(),
-    timestamp :: erlang:timestamp(),
-    sent :: boolean()}).
--record(get_transfer_events_since, {
-    since :: non_neg_integer(),
-    receiver_pid :: pid()
-}).
+-record(account, {account_number :: account_number(), amount :: money()}).
+-record(transfer,
+        {id :: unique_id(),
+         timestamp :: erlang:timestamp(),
+         from_account_number :: account_number(),
+         to_account_number :: account_number(),
+         amount :: money()}).
+-record(transfer_event,
+        {source :: transfer_service,
+         eventId :: non_neg_integer(),
+         accountIdSender :: non_neg_integer(),
+         accountIdReceiver :: non_neg_integer(),
+         amount :: number(),
+         timestamp :: erlang:timestamp(),
+         sent :: boolean()}).
+-record(get_transfer_events_since, {since :: non_neg_integer(), receiver_pid :: pid()}).
