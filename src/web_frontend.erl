b3970f64ae346f7a6eebb5ba8e61dd92d858dc4f
Matthias Quass <matthias@matthiasuquass.de>
2023-09-20 09:26:48 +0200

    Removed Account & Report from web_frontend.erl

--- src/web_frontend.erl
+++ src/web_frontend.erl
@@ -4,28 +4,6 @@
 -export([init/2]).
 
 
-
-account_opened_success() ->
-    << "
-      <p> Account with account number ~p was opened successfully </p> ~n
-       <a href=\"/\"> Back </a>
-    " >>.
-
-
-account_open_form() ->
-            << "
-<h3> Open Account </h3>
-               <form method=\"post\" action=\"/accounts/open\">
-  <label for=\"accounts_givenName\"> Given Name </label>
-  <input type=\"text\" id=\"accounts_givenName\" name=\"accounts_givenName\" />
-
-  <label for=\"accounts_surname\"> Surname </label>
-  <input type=\"text\" id=\"accounts_surname\" name=\"accounts_surname\" />
-
-  <input type=\"submit\" value=\"Open account\" />
-</form>" >>.
-
-
 -spec bin_to_int(binary()) -> integer().
 bin_to_int(B) ->
     erlang:list_to_integer(binary:bin_to_list(B)).
@@ -44,7 +22,7 @@ transfer_success() ->
                <a href=\"/\"> Back </a>
     " >>.
 
-
+-spec transfer_form() -> binary().
 transfer_form() ->
                     << "
 <h3> Create transfer </h3>
@@ -59,49 +37,6 @@ transfer_form() ->
   <input type=\"text\" id=\"transfers_amount\" name=\"transfers_amount\" />
 
   <input type=\"submit\" value=\"Create transfer\" />
-</form>" >>.
-
-
-
-statement_form() ->
-    << "
-<h3> Request statement </h3>
-  <form method=\"post\" action=\"/statements/request\">
-  <table>
-       <tr>
-       <td>
-         <label for=\"statement_accountnumber\"> Account number </label>
-       </td>
-       <td>
-         <input type=\"text\" id=\"statement_accountnumber\" 
-                name=\"statement_accountnumber\" />
-       </td>
-       </tr>
-
-       <tr>
-       <td>
-         <label for=\"statement_currency\"> Currency </label>
-       </td>
-       <td>
-         <input type=\"text\" id=\"statement_currency\" name=\"statement_currency\"
-                value=\"EUR\" size=3/>
-       </td>
-       </tr>
-
-       <tr>
-       <td>
-         <label for=\"statement_format\"> Number & date formatting </label>
-       </td>
-       <td>
-         <select id=\"statement_format\" name=\"statement_format\">
-           <option value=\"de\" selected>DE</option>
-           <option value=\"en\">EN</option>
-         </select>
-       </td>
-       </tr>
-   </table>
-
-  <input type=\"submit\" value=\"Request statement\" />
 </form>" >>.
 
 
@@ -178,41 +113,8 @@ statement(Account, Transfers, Currency, Format) ->
 
 
 index() ->
-    io_lib:format("~s~s~s",
-                  [account_open_form(),
-                   transfer_form(),
-                   statement_form()]).
-
-%% /statements/request
-init(Request, request_statement) ->
-
-    {ok, KeyValuesL, _} = cowboy_req:read_urlencoded_body(Request),
-
-    KeyValues = maps:from_list(KeyValuesL),
-    AccountNumber = bin_to_int(maps:get(<<"statement_accountnumber">>, KeyValues)),
-    Currency = binary_to_list(maps:get(<<"statement_currency">>, KeyValues)),
-    Format = list_to_atom(
-               binary_to_list(maps:get(<<"statement_format">>, KeyValues))),
-
-    logger:info("Bank statement request: account number ~p~n", [AccountNumber]),
-    Ret = business_logic:get_account(AccountNumber),
-    KnowsCurrency = exchange_service:knows_currency(Currency),
-    case {KnowsCurrency, Ret} of
-        {true, {ok, Account}} ->
-            Transfers = business_logic:get_transfers(AccountNumber),
-            Body = statement(Account, Transfers, Currency, Format),
-            Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
-                                    Body, Request),
-            {ok, Reply, []};
-        {false, _} ->
-            Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
-                                    "Currency not found.<br/>" ++ back_button(), Request),
-            {ok, Reply, []};
-        {_, {error, not_found}} ->
-            Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
-                                    "Account not found.<br/>" ++ back_button(), Request),
-            {ok, Reply, []}
-    end;
+    io_lib:format("~s",
+                  [transfer_form()]).
 
 
 %% /transfers/create
@@ -234,25 +136,6 @@ init(Request, create_transfer) ->
     Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Body, Request),
     {ok, Reply, []};
 
-%% /accounts/open
-init(Request, open_account) ->
-
-    logger:info("Creating new account"),
-
-    {ok, KeyValuesL, _} = cowboy_req:read_urlencoded_body(Request),
-
-    KeyValues = maps:from_list(KeyValuesL),
-    GivenName = maps:get(<<"accounts_givenName">>, KeyValues),
-    Surname = maps:get(<<"accounts_surname">>, KeyValues),
-
-    Account = business_logic:open_account(GivenName, Surname),
-    Body = io_lib:format(account_opened_success(), [Account#account.account_number]),
-
-    Reply = cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>}, Body, Request),
-
-    logger:info("Created account with account number ~p", [Account#account.account_number]),
-
-    {ok, Reply, []};
 
 %% /index
 init(Request, index) ->
