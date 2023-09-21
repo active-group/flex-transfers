a84d0522605846fbec1c749dca312e2a43d00d0f
camelmann <68893386+camelmann@users.noreply.github.com>
2023-09-21 15:25:51 +0200

    implement auto ask each 5 sec from account service

--- src/receiveService.erl
+++ src/receiveService.erl
@@ -7,6 +7,7 @@
 -export([handle_cast/2]).
 -export([handle_call/3]).
 -export([start/1]).
+-export([handle_info/2]).
 
 -behaviour(gen_server).
 
@@ -18,21 +19,25 @@ start(AccountNode) ->
     PID.
 
 -spec init({ number(), node()}) -> {ok, state()}.
-init({LastKnownEvent, Node}) ->
+init({_LastKnownEvent, Node}) ->
+    timer:send_interval(5000, alarm),
     Events = events:get_all_events(),
     case Events of
        [] -> logger:info("sending request to account service since ~p", [0]),
-             gen_server:cast({accounts, Node}, #get_account_events_since{since = 0, receiver_pid =self()}),
              {ok, {0, Node}};
        _ -> #event{number = LastKnownEventFromDB} = lists:last(Events),
              logger:info("sending request to account service since ~p", [LastKnownEventFromDB]),
-             gen_server:cast({accounts, Node}, #get_account_events_since{since = LastKnownEventFromDB, receiver_pid =self()}),
              {ok, {LastKnownEventFromDB, Node}}            
     end.
 
+handle_info(alarm, {LastKnownEventFromDB, Node}) -> 
+    gen_server:cast({accounts, Node}, #get_account_events_since{since = LastKnownEventFromDB, receiver_pid =self()}),
+    logger:info("sending request to account service since ~p", [LastKnownEventFromDB]),
+    {noreply, {LastKnownEventFromDB, Node}}.
+
 -spec handle_cast(#account_event{}, state()) -> {noreply, state()}.
 handle_cast(#account_event{account_number = AccountNumber} = Message, _State) ->
-    logger:info("processing event, ~p", Message),
+    logger:info("processing event, ~p", [Message]),
     #event{number = LastKnownEvent} = events:put_event(Message),
     #account{account_number = AccountNumber} = business_logic:open_account_with_account_number(AccountNumber),
     events:put_event(#transfer_event{
