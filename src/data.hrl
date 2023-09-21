3623c371d166e34a21f03b26a1a7536cfaee63cb
Matthias Quass <matthias@matthiasuquass.de>
2023-09-21 14:39:47 +0200

    Added event number

--- src/data.hrl
+++ src/data.hrl
@@ -10,7 +10,8 @@
          to_account_number :: account_number(),
          amount :: money()}).
 -record(transfer_event,
-        {source :: transfer_service,
+        {eventId :: non_neg_integer(),
+         source :: transfer_service,
          accountIdSender :: non_neg_integer(),
          accountIdReceiver :: non_neg_integer(),
          amount :: number(),
