4c4151980878d2afda4f0f28c53c7886d0f2deb1
camelmann <68893386+camelmann@users.noreply.github.com>
2023-09-21 11:54:29 +0200

    fix event passing

--- src/commsService.erl
+++ src/commsService.erl
@@ -1,12 +1,16 @@
 -module(commsService).
 
 -include("data.hrl").
+-include("events.hrl").
 
 -export([init/1]).
 -export([handle_cast/2]).
 -export([handle_call/3]).
 -export([start/0]).
 -export([make_transaction/0]).
+-export([sendEvent/2]).
+-export([handle_events/1]).
+-export([send_events/2]).
 
 -behaviour(gen_server).
 
@@ -23,25 +27,34 @@ init(State) ->
 -spec handle_cast(#get_transfer_events_since{} | send, state()) -> {noreply, state()}.
 handle_cast(send, State) ->
     logger:info("Request to send logs received."),
-    send_events(State),
+    handle_events(State),
     {noreply, []};
 handle_cast(#get_transfer_events_since{} = Message, State) ->
     logger:info("Adding request to state queue, ~s", [Message]),
     {noreply, [Message | State]}.
 
--spec send_events(list(#get_transfer_events_since{})) -> ok.
-send_events(List) ->
+-spec handle_events(list(#get_transfer_events_since{})) -> ok.
+handle_events(List) ->
     logger:info("Sending ~p", [List]),
-    lists:foreach(fun({since = Since, receiver_pid = PID}) ->
-                     sendEvent(events:get_events_from(Since), PID)
+    lists:foreach(fun(#get_transfer_events_since{since = Since, receiver_pid = PID}) ->
+                    send_events(events:get_events_from(Since), PID)
                   end,
                   List),
     ok.
 
--spec sendEvent(#transfer_event{}, pid()) -> ok.
-sendEvent(#transfer_event{} = Event, PID) ->
-    logger:info("Event ~s to PID ~s", [Event, PID]),
-    PID ! Event,
+-spec send_events(list(#event{}), pid()) -> ok.
+send_events(List, PID) ->
+    logger:info("Sending ~p", [List]),
+    lists:foreach(fun(Event) ->
+                     sendEvent(Event, PID)
+                  end,
+                  List),
+    ok.
+
+-spec sendEvent(#event{}, pid()) -> ok.
+sendEvent(#event{payload = EventMessage}, PID) ->
+    logger:info("Event ~pto PID ~p", [EventMessage, PID]),
+    PID ! EventMessage,
     ok.
 
 handle_call(_Message, _From, State) ->
