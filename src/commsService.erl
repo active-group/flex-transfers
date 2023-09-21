87e56b0b7c36f4f096e69edb2f28e0b10f0c34d9
camelmann <68893386+camelmann@users.noreply.github.com>
2023-09-21 10:26:23 +0200

    current stand commsService

--- src/commsService.erl
+++ src/commsService.erl
@@ -1,13 +1,41 @@
 -module(commsService).
+
+-include("data.hrl").
+
 -export([init/1]).
 -export([handle_cast/2]).
 -export([handle_call/3]).
+-export([start/0]).
+
 -behaviour(gen_server).
 
--spec init(non_neg_integer()) -> {ok, non_neg_integer()}.
-init(LastEventId) -> {ok, LastEventId}.
+-type state() :: list(#get_transfer_events_since{}).
 
-handle_cast(Message, State) ->ok.
+start() -> gen_server:start(sender, [], [{debug, [trace]}]).
 
-handle_call(get, _From, State) -> ok.
+-spec init(list(#get_transfer_events_since{})) -> {ok, state()}.
+init(State) ->
+    {ok, State}.
 
+-spec handle_cast(#get_transfer_events_since{} | send, state()) -> {noreply, state()}.
+handle_cast(send, State) ->
+    send_events(State),
+    {noreply, []};
+handle_cast(#get_transfer_events_since{} = Message, State) ->
+    {noreply, [Message | State]}.
+
+-spec send_events(list(#get_transfer_events_since{})) -> ok.
+send_events(List) ->
+    lists:foreach(fun({since = Since, receiver_pid = PID}) ->
+                     sendEvent(events:get_events_from(Since), PID)
+                  end,
+                  List),
+    ok.
+
+-spec sendEvent(#transfer_event{}, pid()) -> ok.
+sendEvent(#transfer_event{} = Event, PID) ->
+    PID ! Event,
+    ok.
+
+handle_call(_Message, _From, State) ->
+    {reply, ok, State}.
