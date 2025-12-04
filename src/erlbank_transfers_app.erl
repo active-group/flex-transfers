22d350f562b35d89c9568b2e282e3a87f09c8a82
Mike Sperber <sperber@deinprogramm.de>
2025-12-04 08:53:49 +0100

    Supply accounts node.

--- src/erlbank_transfers_app.erl
+++ src/erlbank_transfers_app.erl
@@ -26,6 +26,10 @@ start_cowboy() ->
 start(_StartType, _StartArgs) ->
     database:init_database(),
     start_cowboy(),
+
+    AccountNode = node_util:node_from_env(accounts, "ACCOUNTS_HOST"),
+    logger:info("Contacting accounts on: ~p~n", [AccountNode]),
+
     {ok, Pid} = event_server:start(),
     register(transfers, Pid),
     erlbank_transfers_sup:start_link().
